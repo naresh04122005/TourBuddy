dac68b2872c3b30aa08925bcd295cdca4048d1ee
Manish <7055ranamanish@gmail.com>
2024-08-26 22:49:25 +0530

    adding call production callBackUri for GoogleAuth

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.CALL_BACK_URI,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
